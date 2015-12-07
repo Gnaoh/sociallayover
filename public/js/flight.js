98940c6071abccc8c8fee681f2284221dca49582
Peter Hoang <phonghoang@gmail.com>
2015-12-07 09:03:13 -0800

    unminified flights js

--- public/js/flight.js
+++ public/js/flight.js
@@ -1,3 +1,91 @@
-//minified for production
+$(document).ready(function() {
+    var appKey = '3e36be5bc315845b2d546c195a3141a4';
+    var appID = '41f1c3c0';
+    var flightstats = {};
 
-$(document).ready(function(){var t="3e36be5bc315845b2d546c195a3141a4",e="41f1c3c0",a={};$("#give_me").click(function(){var s=$("#airline").val(),d=$("#flight_num").val(),l=(new Date).getFullYear(),r=(new Date).getMonth()+1,u=(new Date).getDate();$.ajax({url:"https://api.flightstats.com/flex/flightstatus/rest/v2/jsonp/flight/status/"+s+"/"+d+"/dep/"+l+"/"+r+"/"+u+"?appId="+e+"&appKey="+t,type:"GET",dataType:"jsonp",success:function(t){for(console.log(t),i=0;i<t.flightStatuses.length;i++)a={},a.airline_code=t.flightStatuses[i].carrierFsCode,a.flight_number=t.flightStatuses[i].flightNumber,a.flightId=t.flightStatuses[i].flightId,a.carrier=t.appendix.airlines[i].name,a.departure_date=t.flightStatuses[i].departureDate.dateUtc,departure=new Date(a.departure_date).toLocaleTimeString(),newDeparture=departure.replace(/:\d\d /," "),a.arrival_date=t.flightStatuses[i].arrivalDate.dateUtc,arrival=new Date(a.arrival_date).toLocaleTimeString(),newArrival=arrival.replace(/:\d\d /," "),a.status=t.flightStatuses[i].status,a.status="S"===a.status?"Scheduled":"A"===a.status?"En Route":"C"===a.status?"Canceled":"D"===a.status?"Diverted":"DN"===a.status?"Data source needed":"NO"===a.status?"Not Operational":"R"===a.status?"Redirected":"L"===a.status?"Landed":"U"===a.status?"Unknown":"CHECK INPUT",t.flightStatuses[i].hasOwnProperty("delays")?($("#delayed_status").html(""),$("#delayed_status").append("<div class='delayed'><p id='status'>Status:<p><p id='statuses'>"+a.status+" - Delayed "+t.flightStatuses[i].delays.arrivalGateDelayMinutes+" minutes</p></div>"),$("#delayed_modal").modal("show")):($("#other_status").html(""),$("#other_status").append("<div class='ontime'><p id='status'>Status:<p><p id='statuses'>"+a.status+" - On-time</p></div>"),$("#delayed_modal").modal("show"));$(".flightstats").html(""),$(".flightstats").append("<tr><td>"+a.airline_code+" "+a.flight_number+"</td><td>"+a.carrier+"</td><td>"+newDeparture+"</td><td>"+newArrival+"</td>")},error:function(t){console.log(t)}})})});
+    $('#give_me').click(function () {
+      var airline = $('#airline').val();
+      var flight_num = $('#flight_num').val();
+      var year = new Date().getFullYear();
+      var month = new Date().getMonth() + 1;
+      var day = new Date().getDate();
+
+/*========================================
+          API INTEGRATION
+========================================*/
+      $.ajax({
+        url: 'https://api.flightstats.com/flex/flightstatus/rest/v2/jsonp/flight/status/' + airline + '/' + flight_num + '/dep/' + year + '/' + month + '/' + day + '?appId=' +appID + '&appKey=' + appKey,
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function (data) {
+          console.log(data);
+            for (i = 0; i < data.flightStatuses.length; i++) {
+              flightstats = {};
+              flightstats.airline_code = data.flightStatuses[i].carrierFsCode;
+              flightstats.flight_number = data.flightStatuses[i].flightNumber;
+              flightstats.flightId = data.flightStatuses[i].flightId;
+              flightstats.carrier = data.appendix.airlines[i].name;
+              flightstats.departure_date = data.flightStatuses[i].departureDate.dateUtc;
+                departure = new Date(flightstats.departure_date).toLocaleTimeString();
+                  newDeparture = departure.replace(/:\d\d /, ' ');                
+              flightstats.arrival_date = data.flightStatuses[i].arrivalDate.dateUtc;
+                arrival = new Date(flightstats.arrival_date).toLocaleTimeString();
+                  newArrival = arrival.replace(/:\d\d /, ' ');
+              flightstats.status = data.flightStatuses[i].status;
+
+/*========================================
+          FLIGHT STATUS CONVERSION
+========================================*/
+            if (flightstats.status === "S") {
+              flightstats.status = "Scheduled";
+            }else if (flightstats.status === "A") {
+              flightstats.status = "En Route"; 
+            }else if (flightstats.status === "C") {
+              flightstats.status = "Canceled";                      
+            }else if (flightstats.status === "D") {
+              flightstats.status = "Diverted"; 
+            }else if (flightstats.status === "DN") {
+              flightstats.status = "Data source needed";
+            }else if (flightstats.status === "NO") {
+              flightstats.status = "Not Operational";
+            }else if (flightstats.status === "R") {
+              flightstats.status = "Redirected";
+            }else if (flightstats.status === "L") {
+              flightstats.status = "Landed";
+            }else if (flightstats.status === "U") {
+              flightstats.status = "Unknown";                 
+            }else {
+              flightstats.status = "CHECK INPUT";
+            }
+
+            // if (data.flightStatuses[i].delays.arrivalGateDelayMinutes === undefined) {
+            //   data.flightStatuses[i].delays.arrivalGateDelayMinutes = "0";
+            // }
+
+ /*========================================
+          FLIGHT MODAL APPEND
+========================================*/           
+              if (data.flightStatuses[i].hasOwnProperty('delays')) {
+              $('#delayed_status').html("");
+              $('#delayed_status').append("<div class='delayed'><p id='status'>Status:<p>" + "<p id='statuses'>" + flightstats.status + " - Delayed " + data.flightStatuses[i].delays.arrivalGateDelayMinutes + " minutes</p></div>");
+              $('#delayed_modal').modal('show');
+              } else {
+              $('#other_status').html("");
+              $('#other_status').append("<div class='ontime'><p id='status'>Status:<p>" + "<p id='statuses'>" + flightstats.status + " - On-time</p></div>");
+              $('#delayed_modal').modal('show');
+              }
+            };
+
+            $('.flightstats').html("");
+            $(".flightstats").append(
+            "<tr><td>" + flightstats.airline_code + " " + flightstats.flight_number +"</td>" +
+            "<td>" + flightstats.carrier +"</td>" +
+            "<td>" + newDeparture +"</td>" + 
+            "<td>" + newArrival +"</td>") 
+            },
+            error: function (error_message) {
+                console.log(error_message);
+             }
+      })
+    });
+});
